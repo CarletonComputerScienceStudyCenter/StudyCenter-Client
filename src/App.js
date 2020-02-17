ee63f1960c173497fc7bb4613f6832dddbb1e04e
MathyouMB <matthewmacraebovell@bell.net>
2020-02-17 17:51:11 -0500

    before app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
+  
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import MathJax from 'react-mathjax-preview'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { gql } from "apollo-boost";
 
-const math = String.raw`
-Consider bitstrings of length 9. The positions in these strings are numbered as $1, 2, 3, ..., 9$. How many such bitstrings have the property that
-<ul>
-    <li>the bit at each even position is 0, or</li>
-    <li>the bitstring starts with 1010?</li>
-    </ul>
 
-<ol type="a">
-		<li>$54$</li>
-		<li>$56$</li>
-        <li>$58$</li>
-        <li>$60$</li>
-	  </ol>`;
 
 function App() {
+
+let [math,setMath] = useState([])
+let [loading, setLoading] = useState(true);
+
+const test = gql`
+{
+  testField{
+    id
+    title
+    question
+  }
+}
+`;
+
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3030/graphql',
+  headers: {
+      "Content-Type": "application/json",
+  }
+})
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+const queryData = async () =>{
+
+    let data = await client
+      .query({
+        query: test,
+      });
+
+
+    setMath(data.data);
+    console.log(data.data.testField[13].question)
+    setLoading(false);
+}
+
   return (
     <div className="App">
       <header className="App-header">
-       <MathJax math={math} />
+        {loading ? "loading..." :
+        
+        math.testField.map( item => (
+          <>
+          <hr></hr>
+          <MathJax math={item.question} />
+          </>
+        ))
+       
+        }
+       <button onClick={queryData} value={"ok"}></button>
       </header>
     </div>
   );
