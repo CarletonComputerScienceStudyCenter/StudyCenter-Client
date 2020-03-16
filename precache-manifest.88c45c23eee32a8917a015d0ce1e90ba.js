c3478d2368ddd55f3f3f781b35bacb8b870c3520
MathyouMB <matthewmacraebovell@bell.net>
2020-03-15 20:56:03 -0400

    Updates

---
+++ precache-manifest.88c45c23eee32a8917a015d0ce1e90ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c371d1e9fd686f2805a7cb4c4255401",
+    "url": "/StudyCenter-Client/index.html"
+  },
+  {
+    "revision": "138687b72c6195dda6b9",
+    "url": "/StudyCenter-Client/static/css/main.f7ec2fe3.chunk.css"
+  },
+  {
+    "revision": "5b53562579e8c821e6a2",
+    "url": "/StudyCenter-Client/static/js/2.2a5ddefd.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/StudyCenter-Client/static/js/2.2a5ddefd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "138687b72c6195dda6b9",
+    "url": "/StudyCenter-Client/static/js/main.ae83ef2c.chunk.js"
+  },
+  {
+    "revision": "cc318aff02abb84b72e3",
+    "url": "/StudyCenter-Client/static/js/runtime-main.4995ec95.js"
+  }
+]);
